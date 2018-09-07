991e96c1bfd81401a119f1d3794e49938058e0d6
Rast <rast@izw-berlin.local>
2018-09-07 14:13:24 +0200

    change _ in the function head for a .

--- R/net_prep.R
+++ R/net_prep.R
@@ -1,9 +1,9 @@
-net_prep = function(data,time,burst){
-by(data,data[,paste(time)],function(chunklet){
-  x <- matrix(chunklet$x,burst,1,byrow = T)
-  y <- matrix(chunklet$y,burst,1,byrow = T)
-  z <- matrix(chunklet$z,burst,1,byrow = T)
-  array(c(x,y,z),dim = c(burst,1,3))}) %>%
+net.prep = function(data,time,burst){
+  by(data,data[,paste(time)],function(chunklet){
+    x <- matrix(chunklet$x,burst,1,byrow = T)
+    y <- matrix(chunklet$y,burst,1,byrow = T)
+    z <- matrix(chunklet$z,burst,1,byrow = T)
+    array(c(x,y,z),dim = c(burst,1,3))}) %>%
   ##bind every list item(arrays) into 1 giant array
   abind(. , along = 4) %>%
   #reorder the dimentions of the array (though I have no idea what that means)
