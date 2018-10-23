7ab78b2eed5ec8cf625f67835e9ba2175b57f921
Rast <rast@izw-berlin.local>
2018-10-23 10:34:20 +0200

    add renaming of the timestamp coulmn to timestamp regards of initial name change group_by_ to group_by becuase the grouping variable is now provided by the fundtion as timestamp every time

--- R/sum_data.R
+++ R/sum_data.R
@@ -1,14 +1,17 @@
 sum.data = function(data,IntDur=NULL,burstcount=NULL,windowstart=1,time,x,y=NULL,z=NULL,ID=NA,Tag.ID=NA,sex=NA,stats){
-  if(burstcount == data %>% group_by(. , time) %>% summarise(n()) %>% dplyr::select(.,2) %>% slice(.,1) &
+
+  names(data)[names(data)==time] <- "timestamp"
+
+  if(burstcount == data %>% group_by(. , timestamp) %>% summarise(n()) %>% dplyr::select(.,2) %>% slice(.,1) &
      windowstart == 1){
-    data <- group_by_(data,time)
+    data <- group_by(data,timestamp)
   }
   else{
-    if(windowstart > data %>% group_by(. , time) %>% summarise(n()) %>% dplyr::select(.,2) %>% slice(.,1)-burstcount){
+    if(windowstart > data %>% group_by(. , timestamp) %>% summarise(n()) %>% dplyr::select(.,2) %>% slice(.,1)-burstcount){
       warning("Window will run out of bounds of the burst. Reduce the burstcount or window staring row." , call. = F)
       stop()
     }
-    data <- group_by_(data,time) %>%
+    data <- group_by(data,timestamp) %>%
       slice(. , windowstart:(windowstart + burstcount - 1))
   }
   names(data)[names(data)==x] <- "x"
