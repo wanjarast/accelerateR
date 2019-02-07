6a33fe5c3fd96ba4e5461d7c3c8fd3680f0ca70e
Rast <rast@izw-berlin.local>
2019-02-07 11:45:13 +0100

    change complete in code to data

--- R/acto.R
+++ R/acto.R
@@ -17,7 +17,7 @@ acto <- function(data , time , date , behaviour , target.bev , daily = FALSE){
       geom_point(data = data , aes(x = Uhrzeit , y = date_numeric , color = color) , shape = 15 , size =8)+
       scale_color_manual("Status", values = c("1" = "black" , "0" = "white"))+ #grey92 als Standard Hintergrundfarbe
       theme(legend.position = "none" ,axis.line = element_line(colour = "black", size = 0.5, linetype = "solid"))+
-      scale_y_reverse(breaks = seq(1 , length(levels(complete$Datum)) , 1) , labels = levels(complete$Datum))+
+      scale_y_reverse(breaks = seq(1 , length(levels(data$Datum)) , 1) , labels = levels(data$Datum))+
       scale_x_discrete(breaks = c("02:00" , "04:00" , "06:00" , "08:00" , "10:00" , "12:00",
                                   "14:00" , "16:00" , "18:00" , "20:00" , "22:00"), labels = c("02:00" , "04:00" , "06:00" , "08:00" , "10:00" , "12:00",
                                                                                                "14:00" , "16:00" , "18:00" , "20:00" , "22:00"))+
@@ -36,7 +36,7 @@ acto <- function(data , time , date , behaviour , target.bev , daily = FALSE){
       geom_point(data = data , aes(x = Uhrzeit , y = date_numeric , color = color) , shape = 15 , size =8)+
       scale_color_manual("Status", values = c("1" = "black" , "0" = "white"))+ #grey92 als Standard Hintergrundfarbe
       theme(legend.position = "none" ,axis.line = element_line(colour = "black", size = 0.5, linetype = "solid"))+
-      scale_y_reverse(breaks = seq(1 , length(levels(complete$Datum)) , 1)[unique.date] , labels = levels(complete$Datum)[unique.date])+
+      scale_y_reverse(breaks = seq(1 , length(levels(data$Datum)) , 1)[unique.date] , labels = levels(data$Datum)[unique.date])+
       scale_x_discrete(breaks = c("02:00" , "04:00" , "06:00" , "08:00" , "10:00" , "12:00",
                                   "14:00" , "16:00" , "18:00" , "20:00" , "22:00"), labels = c("02:00" , "04:00" , "06:00" , "08:00" , "10:00" , "12:00",
                                                                                                "14:00" , "16:00" , "18:00" , "20:00" , "22:00"))+
