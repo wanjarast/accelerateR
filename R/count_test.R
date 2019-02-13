2ba188b5a9fab59bae8c55e9d2bde3272eef960e
Rast <rast@izw-berlin.local>
2019-02-13 11:21:17 +0100

    change time to timestamp for the grouping

--- R/count_test.R
+++ R/count_test.R
@@ -2,7 +2,7 @@ count.test <- function(data,burstcount,time="timestamp"){
   names(data)[names(data) == time] <- "timestamp"
 
   df <- data %>%
-    dplyr::group_by(time) %>%
+    dplyr::group_by(timestamp) %>%
     dplyr::summarise(. , n()==burstcount)
   df_false <- df[df$`n() == burstcount`==FALSE,1]
 
