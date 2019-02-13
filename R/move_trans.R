fdf00dc7996440886ea638900f095dc1b8b0ea4b
Rast <rast@izw-berlin.local>
2019-02-13 10:16:32 +0100

    add acc to the help file to give the acc coloumn name to the function add requirement to the function to put in the name of the acc column remove addinotianl data column from the uptput

--- R/move_trans.R
+++ R/move_trans.R
@@ -1,6 +1,6 @@
-move.trans <- function(data,time="timestamp",burst,naxes=3){
-  names(data) <- gsub(pattern = ":" , replacement = "." , x = names(data))
-  names(data) <- gsub(pattern = "-" , replacement = "." , x = names(data))
+move.trans <- function(data,time="timestamp",acc="eobs:accelerations-raw",burst,naxes=3){
+  names(data)[names(data) == acc] <- "eobs.accelerations.raw"
+
   if(naxes == 3){
   reorder.acc <- function(rows){
     df <-data.frame(x=rep(NA,nrow(rows)*burst),y=NA,z=NA,taxon_name=NA,tag_id=NA,individual_id=NA,study_name=NA)
@@ -8,10 +8,7 @@ move.trans <- function(data,time="timestamp",burst,naxes=3){
     df$x <- V[,1]
     df$y <- V[,2]
     df$z <- V[,3]
-    df$taxon_name <- rows$individual.taxon.canonical.name
-    df$tag_id <- rows$tag.local.identifier
-    df$individual_id <- rows$individual.local.identifier
-    df$study_name <- rows$study.name
+
     return(df)
   }
   output <- data %>%
@@ -25,10 +22,6 @@ move.trans <- function(data,time="timestamp",burst,naxes=3){
       V=matrix(sapply(unlist(strsplit(as.character(rows$eobs.accelerations.raw)," ")),as.numeric),byrow=T,ncol=2)
       df$x <- V[,1]
       df$y <- V[,2]
-      df$taxon_name <- rows$individual.taxon.canonical.name
-      df$tag_id <- rows$tag.local.identifier
-      df$individual_id <- rows$individual.local.identifier
-      df$study_name <- rows$study.name
       return(df)
     }
     output <- data %>%
@@ -41,10 +34,6 @@ move.trans <- function(data,time="timestamp",burst,naxes=3){
       df <-data.frame(x=rep(NA,nrow(rows)*burst),taxon_name=NA,tag_id=NA,individual_id=NA,study_name=NA)
       V=matrix(sapply(unlist(strsplit(as.character(rows$eobs.accelerations.raw)," ")),as.numeric),byrow=T,ncol=1)
       df$x <- V[,1]
-      df$taxon_name <- rows$individual.taxon.canonical.name
-      df$tag_id <- rows$tag.local.identifier
-      df$individual_id <- rows$individual.local.identifier
-      df$study_name <- rows$study.name
       return(df)
     }
     output <- data %>%
