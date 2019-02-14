1abf7505f0ebc1a13fe69d6fcbde41b7b192e28d
Rast <rast@izw-berlin.local>
2019-02-14 14:53:58 +0100

    add sunset/sunrise line options to acto and acot.odba updat acto and acto.odba help files

--- R/acto.R
+++ R/acto.R
@@ -1,10 +1,23 @@
-acto <- function(data , time , behaviour , target.bev , daily = FALSE , night.shift = F){
+acto <- function(data , time , behaviour , target.bev , daily = FALSE , night.shift = F , sun = F , suncolor = "red" , timezone = "UTC"){
   data <- data
   names(data)[names(data) == time] <- "timestamp"
   names(data)[names(data) == behaviour] <- "Verhalten"
 
   data$timestamp <- ymd_hms(data$timestamp)
   data$Datum <- as.Date(data$timestamp)
+
+  if(sun == T){
+    sunrise <- maptools::sunriset(crds = matrix(c(13.31,52.30) , nrow = 1) ,
+                                  dateTime = as_datetime(data$timestamp) ,
+                                  POSIXct.out = T ,
+                                  direction = "sunrise")$time
+
+    sunset <- maptools::sunriset(crds = matrix(c(13.31,52.30) , nrow = 1) ,
+                                 dateTime = as_datetime(data$timestamp) ,
+                                 POSIXct.out = T ,
+                                 direction = "sunset")$time
+  }
+
   data$Verhalten <- factor(data$Verhalten)
   data$color <- ifelse(data$Verhalten %in% target.bev , 1 , 0)
   data$color <- factor(data$color)
@@ -12,45 +25,109 @@ acto <- function(data , time , behaviour , target.bev , daily = FALSE , night.sh
   data$date_numeric <- as.numeric(data$Datum)
 
   if(night.shift == T){
-    data$Uhrzeit <- strftime(as_datetime(data$timestamp) + hours(12), format="%H:%M" , tz = "UTC")
-  }
-
-  else{
-    data$Uhrzeit <- strftime(data$timestamp, format="%H:%M" , tz = "UTC")
-  }
-
-
-  if(daily == TRUE){
-
-    ggplot()+
-      geom_point(data = data , aes(x = Uhrzeit , y = date_numeric , color = color) , shape = 15 , size =8)+
-      scale_color_manual("Status", values = c("1" = "black" , "0" = "white"))+ #grey92 als Standard Hintergrundfarbe
-      theme(legend.position = "none" ,axis.line = element_line(colour = "black", size = 0.5, linetype = "solid"))+
-      scale_y_reverse(breaks = seq(1 , length(levels(data$Datum)) , 1) , labels = levels(data$Datum))+
-      scale_x_discrete(breaks = sort(unique(data$Uhrzeit))[round(seq(1 , length(unique(data$Uhrzeit)) , length.out = 9))],
-                       labels = c("" , "03:00" , "06:00" , "09:00" , "12:00" , "15:00" , "18:00" , "21:00" , "" ))+
-      ylab(label = "date")+
-      xlab(label = "Time of the day")+
-      theme(panel.background = element_blank())
-
-  }
-  else{
-    unique.date <- c(TRUE , rep(NA,length(lubridate::month(lubridate::ymd(levels(data$Datum))))-1))
-
-    for(i in 2:length(lubridate::month(lubridate::ymd(levels(data$Datum))))){
-      unique.date[i] <- !(identical(lubridate::month(lubridate::ymd(levels(data$Datum)))[i],month(lubridate::ymd(levels(data$Datum)))[i-1]))
+    data$Uhrzeit <- strftime(as_datetime(data$timestamp) + hours(12), format="%H:%M" , tz = timezone)
+    if(sun == T){
+      data$sunrise <- strftime(sunrise + hours(12), format="%H:%M" , tz = timezone)
+      data$sunset <- strftime(sunset + hours(12), format="%H:%M" , tz = timezone)
     }
-
-    ggplot()+
-      geom_tile(data = data , aes(x = Uhrzeit , y = date_numeric , color = color) , size =1)+
-      scale_color_manual("Status", values = c("1" = "black" , "0" = "white"))+ #grey92 als Standard Hintergrundfarbe
-      theme(legend.position = "none" ,axis.line = element_line(colour = "black", size = 0.5, linetype = "solid"))+
-      scale_y_reverse(breaks = seq(1 , length(levels(data$Datum)) , 1)[unique.date] , labels = levels(data$Datum)[unique.date])+
-      scale_x_discrete(breaks = sort(unique(data$Uhrzeit))[round(seq(1 , length(unique(data$Uhrzeit)) , length.out = 9))],
-                       labels = c("" , "03:00" , "06:00" , "09:00" , "12:00" , "15:00" , "18:00" , "21:00" , "" ))+
-      ylab(label = "date")+
-      xlab(label = "Time of the day")+
-      theme(panel.background = element_blank())
   }
 
+  else{
+    data$Uhrzeit <- strftime(data$timestamp, format="%H:%M" , tz = timezone)
+    if(sun == T){
+      data$sunrise <- strftime(sunrise, format="%H:%M" , tz = timezone)
+      data$sunset <- strftime(sunset, format="%H:%M" , tz = timezone)
+    }
+  }
+
+  unique.date <- c(TRUE , rep(NA,length(lubridate::month(lubridate::ymd(levels(data$Datum))))-1))
+
+  for(i in 2:length(lubridate::month(lubridate::ymd(levels(data$Datum))))){
+    unique.date[i] <- !(identical(lubridate::month(lubridate::ymd(levels(data$Datum)))[i],month(lubridate::ymd(levels(data$Datum)))[i-1]))
+  }
+
+  basis <- ggplot()+
+    geom_tile(data = data , aes(x = Uhrzeit , y = date_numeric , color = color) , size =1.1)+
+    theme(legend.position = "none" ,axis.line = element_line(colour = "black", size = 0.5, linetype = "solid"))+
+    ylab(label = "date")+
+    xlab(label = "Time of the day")+
+    theme(panel.background = element_blank())
+
+  if(night.shift == T){
+    if(daily != T & sun != T){
+      basis+
+        scale_color_manual("Status", values = c("1" = "black" , "0" = "white"))+
+        scale_y_reverse(breaks = seq(1 , length(levels(data$Datum)) , 1)[unique.date] , labels = levels(data$Datum)[unique.date])+
+        scale_x_discrete(breaks = sort(unique(data$Uhrzeit))[round(seq(1 , length(unique(data$Uhrzeit)) , length.out = 9))],
+                         labels = c("" , "15:00" , "18:00" , "21:00" , "00:00" , "03:00" , "06:00" , "09:00" , "" ))
+    }
+    else if(daily != T & sun == T){
+      basis+
+        scale_y_reverse(breaks = seq(1 , length(levels(data$Datum)) , 1)[unique.date] , labels = levels(data$Datum)[unique.date])+
+        scale_x_discrete(breaks = sort(unique(data$Uhrzeit))[round(seq(1 , length(unique(data$Uhrzeit)) , length.out = 9))],
+                         labels = c("" , "15:00" , "18:00" , "21:00" , "00:00" , "03:00" , "06:00" , "09:00" , "" ))+
+        geom_line(data = data[data$sunrise %in% data$Uhrzeit,] , aes(x = sunrise , y = date_numeric ,
+                                                                     color = "sunrise/sunset" , group = 1),size = 1.5 , alpha = 0.7)+
+        geom_line(data = data[data$sunset %in% data$Uhrzeit,] , aes(x = sunset , y = date_numeric ,
+                                                                    color = "sunrise/sunset" , group = 1),size = 1.5 , alpha = 0.7)+
+        scale_color_manual("Status", values = c("1" = "black" , "0" = "white" , "sunrise/sunset" = suncolor))
+    }
+    else if(daily == T & sun == T){
+      basis+
+        scale_y_reverse(breaks = seq(1 , length(levels(data$Datum)) , 1) , labels = levels(data$Datum))+
+        scale_x_discrete(breaks = sort(unique(data$Uhrzeit))[round(seq(1 , length(unique(data$Uhrzeit)) , length.out = 9))],
+                         labels = c("" , "15:00" , "18:00" , "21:00" , "00:00" , "03:00" , "06:00" , "09:00" , "" ))+
+        geom_line(data = data[data$sunrise %in% data$Uhrzeit,] , aes(x = sunrise , y = date_numeric ,
+                                                                     color = "sunrise/sunset" , group = 1),size = 1.5 , alpha = 0.7)+
+        geom_line(data = data[data$sunset %in% data$Uhrzeit,] , aes(x = sunset , y = date_numeric ,
+                                                                    color = "sunrise/sunset" , group = 1),size = 1.5 , alpha = 0.7)+
+        scale_color_manual("Status", values = c("1" = "black" , "0" = "white" , "sunrise/sunset" = suncolor))
+    }
+    else if(daily == T & sun != T){
+      basis+
+        scale_color_manual("Status", values = c("1" = "black" , "0" = "white"))+
+        scale_y_reverse(breaks = seq(1 , length(levels(data$Datum)) , 1) , labels = levels(data$Datum))+
+        scale_x_discrete(breaks = sort(unique(data$Uhrzeit))[round(seq(1 , length(unique(data$Uhrzeit)) , length.out = 9))],
+                         labels = c("" , "15:00" , "18:00" , "21:00" , "00:00" , "03:00" , "06:00" , "09:00" , "" ))
+    }
+  }
+
+  else{
+    if(daily == T & sun != T){
+      basis+
+        scale_color_manual("Status", values = c("1" = "black" , "0" = "white"))+
+        scale_x_discrete(breaks = sort(unique(data$Uhrzeit))[round(seq(1 , length(unique(data$Uhrzeit)) , length.out = 9))],
+                         labels = c("" , "03:00" , "06:00" , "09:00" , "12:00" , "15:00" , "18:00" , "21:00" , "" ))+
+        scale_y_reverse(breaks = seq(1 , length(levels(data$Datum)) , 1) , labels = levels(data$Datum))
+    }
+    else if(daily != T & sun != T){
+      basis+
+        scale_color_manual("Status", values = c("1" = "black" , "0" = "white"))+
+        scale_x_discrete(breaks = sort(unique(data$Uhrzeit))[round(seq(1 , length(unique(data$Uhrzeit)) , length.out = 9))],
+                         labels = c("" , "03:00" , "06:00" , "09:00" , "12:00" , "15:00" , "18:00" , "21:00" , "" ))+
+        scale_y_reverse(breaks = seq(1 , length(levels(data$Datum)) , 1)[unique.date] , labels = levels(data$Datum)[unique.date])
+    }
+    else if(daily == T & sun == T){
+      basis+
+        scale_x_discrete(breaks = sort(unique(data$Uhrzeit))[round(seq(1 , length(unique(data$Uhrzeit)) , length.out = 9))],
+                         labels = c("" , "03:00" , "06:00" , "09:00" , "12:00" , "15:00" , "18:00" , "21:00" , "" ))+
+        geom_line(data = data[data$sunrise %in% data$Uhrzeit,] , aes(x = sunrise , y = date_numeric ,
+                                                                     color = "sunrise/sunset" , group = 1),size = 1.5 , alpha = 0.7)+
+        geom_line(data = data[data$sunset %in% data$Uhrzeit,] , aes(x = sunset , y = date_numeric ,
+                                                                    color = "sunrise/sunset" , group = 1),size = 1.5 , alpha = 0.7)+
+        scale_color_manual("Status", values = c("1" = "black" , "0" = "white" , "sunrise/sunset" = suncolor))+
+        scale_y_reverse(breaks = seq(1 , length(levels(data$Datum)) , 1) , labels = levels(data$Datum))
+    }
+    else if(daily != T & sun == T){
+      basis+
+        scale_x_discrete(breaks = sort(unique(data$Uhrzeit))[round(seq(1 , length(unique(data$Uhrzeit)) , length.out = 9))],
+                         labels = c("" , "03:00" , "06:00" , "09:00" , "12:00" , "15:00" , "18:00" , "21:00" , "" ))+
+        geom_line(data = data[data$sunrise %in% data$Uhrzeit,] , aes(x = sunrise , y = date_numeric ,
+                                                                     color = "sunrise/sunset" , group = 1),size = 1.5 , alpha = 0.7)+
+        geom_line(data = data[data$sunset %in% data$Uhrzeit,] , aes(x = sunset , y = date_numeric ,
+                                                                    color = "sunrise/sunset" , group = 1),size = 1.5 , alpha = 0.7)+
+        scale_color_manual("Status", values = c("1" = "black" , "0" = "white" , "sunrise/sunset" = suncolor))+
+        scale_y_reverse(breaks = seq(1 , length(levels(data$Datum)) , 1)[unique.date] , labels = levels(data$Datum)[unique.date])
+    }
+  }
 }
--- R/acto_odba.R
+++ R/acto_odba.R
@@ -1,4 +1,4 @@
-acto.odba <- function(data , time , ODBA = "ODBA" , cutoff = 1 , night.shift = F){
+acto.odba <- function(data , time , ODBA = "ODBA" , cutoff = 1 , night.shift = F , sun = F , suncolor = "red" , timezone = "UTC"){
   data <- data
   names(data)[names(data) == time] <- "timestamp"
   names(data)[names(data) == ODBA] <- "ODBA"
@@ -7,13 +7,32 @@ acto.odba <- function(data , time , ODBA = "ODBA" , cutoff = 1 , night.shift = F
 
   data$Datum <- as.Date(data$timestamp)
 
+  if(sun == T){
+    sunrise <- maptools::sunriset(crds = matrix(c(13.31,52.30) , nrow = 1) ,
+                                  dateTime = as_datetime(data$timestamp) ,
+                                  POSIXct.out = T ,
+                                  direction = "sunrise")$time
+
+    sunset <- maptools::sunriset(crds = matrix(c(13.31,52.30) , nrow = 1) ,
+                                 dateTime = as_datetime(data$timestamp) ,
+                                 POSIXct.out = T ,
+                                 direction = "sunset")$time
+  }
 
   if(night.shift == T){
-    data$Uhrzeit <- strftime(as_datetime(data$timestamp) + hours(12), format="%H:%M" , tz = "UTC")
+    data$Uhrzeit <- strftime(as_datetime(data$timestamp) + hours(12), format="%H:%M" , tz = timezone)
+    if(sun == T){
+      data$sunrise <- strftime(sunrise + hours(12), format="%H:%M" , tz = timezone)
+      data$sunset <- strftime(sunset + hours(12), format="%H:%M" , tz = timezone)
+    }
   }
 
   else{
-    data$Uhrzeit <- strftime(data$timestamp, format="%H:%M" , tz = "UTC")
+    data$Uhrzeit <- strftime(data$timestamp, format="%H:%M" , tz = timezone)
+    if(sun == T){
+      data$sunrise <- strftime(sunrise, format="%H:%M" , tz = timezone)
+      data$sunset <- strftime(sunset, format="%H:%M" , tz = timezone)
+    }
   }
 
   data$Datum <- factor(as.character(data$Datum) , levels = unique(as.character(data$Datum)))
@@ -30,20 +49,30 @@ acto.odba <- function(data , time , ODBA = "ODBA" , cutoff = 1 , night.shift = F
   }
 
   if(night.shift == T){
-    ggplot()+
-      geom_tile(data = data , aes(x = Uhrzeit , y = date_numeric , fill = ODBA) , size =1)+
+    shift <- ggplot(data = data)+
+      geom_tile(aes(x = Uhrzeit , y = date_numeric , fill = ODBA) , size =1)+
       theme(axis.line = element_line(colour = "black", size = 0.5, linetype = "solid"))+
       scale_y_reverse(breaks = seq(1 , length(levels(data$Datum)) , 1)[unique.date] , labels = levels(data$Datum)[unique.date])+
+
       scale_x_discrete(breaks = sort(unique(data$Uhrzeit))[round(seq(1 , length(unique(data$Uhrzeit)) , length.out = 9))],
                        labels = c("" , "15:00" , "18:00" , "21:00" , "00:00" , "03:00" , "06:00" , "09:00" , "" ))+
       scale_fill_gradient(low = "white", high = "black")+
       ylab(label = "date")+
       xlab(label = "Time of the day")+
       theme(panel.background = element_blank())
+    if(sun == T){
+      shift+
+        geom_line(data = data[data$sunrise %in% data$Uhrzeit,] , aes(x = sunrise , y = date_numeric ,
+                                                                     color = "sunrise/sunset" , group = 1),size = 1.5 , alpha = 0.7)+
+        geom_line(data = data[data$sunset %in% data$Uhrzeit,] , aes(x = sunset , y = date_numeric ,
+                                                                    color = "sunrise/sunset" , group = 1),size = 1.5 , alpha = 0.7)+
+        scale_color_manual(values = c("sunrise/sunset" = suncolor))
+    }
+    else{shift}
   }
   else{
-    ggplot()+
-      geom_tile(data = data , aes(x = Uhrzeit , y = date_numeric , fill = ODBA) , size =1)+
+    noshift <- ggplot(data = data)+
+      geom_tile(aes(x = Uhrzeit , y = date_numeric , fill = ODBA) , size =1)+
       theme(axis.line = element_line(colour = "black", size = 0.5, linetype = "solid"))+
       scale_y_reverse(breaks = seq(1 , length(levels(data$Datum)) , 1)[unique.date] , labels = levels(data$Datum)[unique.date])+
       scale_x_discrete(breaks = sort(unique(data$Uhrzeit))[round(seq(1 , length(unique(data$Uhrzeit)) , length.out = 9))],
@@ -52,6 +81,14 @@ acto.odba <- function(data , time , ODBA = "ODBA" , cutoff = 1 , night.shift = F
       ylab(label = "date")+
       xlab(label = "Time of the day")+
       theme(panel.background = element_blank())
+    if(sun == T){
+      noshift+
+        geom_line(data = data[data$sunrise %in% data$Uhrzeit,] , aes(x = sunrise , y = date_numeric ,
+                                                                     color = "sunrise/sunset" , group = 1),size = 1.5 , alpha = 0.7)+
+        geom_line(data = data[data$sunset %in% data$Uhrzeit,] , aes(x = sunset , y = date_numeric ,
+                                                                    color = "sunrise/sunset" , group = 1),size = 1.5 , alpha = 0.7)+
+        scale_color_manual(values = c("sunrise/sunset" = suncolor))
+    }
+    else(noshift)
   }
-
 }
