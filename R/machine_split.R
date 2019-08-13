c7976df0eb26199c716e2ba3eeb96191aa5699cc
Wanja Rast <wanja.home@t-online.de>
2019-08-13 16:03:29 +0200

    add dplyr functionality to machine split when selecting subsamples now better respects behavour proportions

--- R/machine_split.R
+++ R/machine_split.R
@@ -1,32 +1,37 @@
-machine.split <- function(data , group , train.size , val.size , seed = 142 , names = c("train.data","val.data","test.data")){
+machine.split <- function(data , group = "timestamp" , behaviour , train.size , val.size , seed = 142 , names = c("train.data","val.data","test.data")){
   set.seed(seed)
   if(train.size + val.size > 1){
     stop("train.size + val.size have to be smaller than 1")
   }
-
   data%>%
-    split(. , .[[group]]) -> splited.data
+    group_by_(. , behaviour , group)%>%
+    nest(.)%>%
+    ungroup(.)%>%
+    group_by_(. , behaviour) -> nested.data
 
-  splited.data[sample(length(unique(data[[group]])) * train.size)]%>%
-    data.table::rbindlist(.)%>%
-    as.data.frame(.) -> train.data
+  nested.data%>%
+    sample_frac( . , size = train.size)%>%
+    ungroup(.)%>%
+    slice(. , sample(nrow(.)))%>%
+    unnest(.)-> train.data
 
-  remaining <- data[!(data[[group]] %in% train.data[[group]]) , ]
-
-  remaining%>%
-    split(. , .[[group]]) -> splited.remaining.data
+  remaining <- nested.data[!nested.data[[group]] %in% train.data[[group]], ]
 
   val.size.calc <- (nrow(data) * val.size) /
     (nrow(data) - train.size*nrow(data))
 
-  splited.remaining.data[sample(length(unique(remaining[[group]])) *
-                                  val.size.calc)]%>%
-    data.table::rbindlist(.)%>%
-    as.data.frame(.) -> val.data
+  remaining%>%
+    sample_frac( . , size = val.size.calc)%>%
+    ungroup(.)%>%
+    slice(. , sample(nrow(.)))%>%
+    unnest(.)-> val.data
+
   if(train.size + val.size < 1){
-    test.data <- data[!(data[[group]] %in% train.data[[group]]) &
-                        !(data[[group]] %in% val.data[[group]]) , ]%>%
-      as.data.frame(.)
+    remaining[!(remaining[[group]] %in% val.data[[group]]), ]%>%
+      ungroup(.)%>%
+      slice(. , sample(nrow(.)))%>%
+      unnest(.)-> test.data
+
     assign(names[3] , test.data , envir = .GlobalEnv)
   }
 
