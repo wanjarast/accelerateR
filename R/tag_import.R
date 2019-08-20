571ebccf86cc71f62f2e71b8ad6498536ef52442
Rast <rast@izw-berlin.local>
2019-08-20 13:03:00 +0200

    change . to _ in tag_import

--- R/tag_import.R
+++ R/tag_import.R
@@ -1,6 +1,6 @@
 tag_import <- function(tag_id , rounding = 10 , eobs = TRUE , data_path = NULL){
   # read all files from the set tag id
-  pat_data <- paste(tag.id,"_acc[[:graph:]]*.txt" , sep="")
+  pat_data <- paste(tag_id,"_acc[[:graph:]]*.txt" , sep="")
 
   if(!is.null(data_path)){
     temp_data <- list.files(path = data_path, pattern = pat_data , full.names = T)
@@ -8,15 +8,15 @@ tag_import <- function(tag_id , rounding = 10 , eobs = TRUE , data_path = NULL){
     temp_data <- list.files(pattern = pat_data)
   }
   if(isTRUE(eobs)){
-    data.list <- lapply(temp_data, fread, drop = c(1,3) , col.names = c("date","time","x","y","z"))
-    data <- data.table::rbindlist(data.list)
+    data_list <- lapply(temp_data, fread, drop = c(1,3) , col.names = c("date","time","x","y","z"))
+    data <- data.table::rbindlist(data_list)
     # create the timestamp coulumn and round the seconds down to full 10s
     data$timestamp <- paste(data$date , data$time , sep = " ")
     data$timestamp <- dmy_hms(data$timestamp)
     data$timestamp <- floor_date(data$timestamp , unit = seconds(x = rounding))
   } else{
-    data.list <- lapply(temp_data, fread)
-    data <- data.table::rbindlist(data.list)
+    data_list <- lapply(temp_data, fread)
+    data <- data.table::rbindlist(data_list)
   }
   return(data)
 }
