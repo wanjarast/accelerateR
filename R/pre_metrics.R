000faf8fb98933bf5911f47647f4c98d15419913
Rast <rast@izw-berlin.local>
2020-03-05 15:20:54 +0100

    add handling of uncertain predictions for calculations of accuracy and kappa

--- R/pre_metrics.R
+++ R/pre_metrics.R
@@ -1,9 +1,24 @@
 pre_metrics = function(predicted , expected , uncertain = NULL){
 
-  conf_table <- table(predicted = predicted , expected = expected) #compute the confusion matrix
+  conf_table_print <- table(predicted = predicted , expected = expected) #compute the confusion matrix
+
+  # remove the row with the "other" predictions as it makes the calculations impossible
+  if(!is.null(uncertain)){
+    conf_table <- conf_table_print[!(rownames(conf_table_print) == uncertain),]
+    other_table <- conf_table_print[(rownames(conf_table_print) == uncertain),]
+  }
+  else{
+    conf_table <- table(predicted = predicted , expected = expected)
+  }
 
   # total number of all classifications (needed for the accuracy)
-  total_n <- sum(conf_table)
+  if(!is.null(uncertain)){
+    total_n <- sum(conf_table) + sum(other_table)
+  }
+  else{
+    total_n <- sum(conf_table)
+  }
+
   correct_sum <- sum(diag(conf_table))
 
   conf_names <- colnames(conf_table) #get the name of the behaviours
@@ -11,14 +26,22 @@ pre_metrics = function(predicted , expected , uncertain = NULL){
   result <- matrix(nrow = 3 , ncol = length(conf_names) , dimnames = list(c("recall" , "precision" , "accuracy"),
                                                                           c(conf_names)))
   if(!(is.null(uncertain))){
-    others <- sum(conf_table[rownames(conf_table) == uncertain])/sum(conf_table)
+    others <- sum(other_table)
   }
 
   for(i in conf_names){
     correct <- conf_table[rownames(conf_table) == i , colnames = i]
     rowsum <- sum(conf_table[rownames(conf_table) == i , ])
+    if(!(is.null(uncertain))){
+      colsum <- sum(conf_table[ , colnames(conf_table) == i] + other_table[names(other_table) == i])
+    }
     colsum <- sum(conf_table[ , colnames(conf_table) == i])
-    true_negatives <- sum(conf_table[rownames(conf_table) != i , colnames(conf_table) != i])
+    if(!(is.null(uncertain))){
+      true_negatives <- sum(conf_table[rownames(conf_table) != i , colnames(conf_table) != i])
+    }
+    else{
+      true_negatives <- sum(conf_table[rownames(conf_table) != i , colnames(conf_table) != i]) + sum(other_table[names(other_table) != i])
+    }
 
     result[rownames(result) == "recall" , colnames(result) == i] <- correct / colsum
     result[rownames(result) == "precision" , colnames(result) == i] <- correct / rowsum
@@ -30,9 +53,9 @@ pre_metrics = function(predicted , expected , uncertain = NULL){
   kappa <- (observed_accuray - expected_accuray) / (1 - expected_accuray)
 
   if(!(is.null(uncertain))){
-  return(list(confusion_matrix = conf_table , metrics = result , kappa = kappa ,uncertain_predictions = others))
+    return(list(confusion_matrix = conf_table_print , metrics = result , kappa = kappa , uncertain_predictions = others))
   }
   if(is.null(uncertain)){
-    return(list(confusion_matrix = conf_table , metrics = result , kappa = kappa))
+    return(list(confusion_matrix = conf_table_print , metrics = result , kappa = kappa))
   }
 }
